cd9adfc925cc0378cfdf2926ff64249c056c4c63
Rakib1415 <rakibiuaece@gmail.com>
2023-09-02 00:32:02 +0600

    feat:(Chart page) get all country data from api

--- src/pages/Chart.svelte
+++ src/pages/Chart.svelte
@@ -1,4 +1,12 @@
 <script>
+    import { countryDataStore } from "../store/store";
+
+    const getCountryData = async() => {
+        const res = await fetch('https://restcountries.com/v3.1/all');
+        const data = await res.json();
+        countryDataStore.set(data);
+    }
+    getCountryData();
   
 </script>
 
--- src/pages/Map.svelte
+++ src/pages/Map.svelte
@@ -1,5 +1,8 @@
 <script>
-  
+  import { countryDataStore } from "../store/store";
+
+  const mostPopulatedCountry = $countryDataStore.sort((a, b) => b.population - a.population);
+  console.log(mostPopulatedCountry.slice(0, 10));
 </script>
 
 <h1 class="text-3xl underline text-bold-">I am a Map Page</h1>
